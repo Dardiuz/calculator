3b9793793b40750cf32042756911a54185ac9125
armando <donnynoorlander@gmail.com>
2023-08-23 23:02:13 +0200

    finished calculator

--- main.js
+++ main.js
@@ -1,34 +1,91 @@
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-let num1 = "";
 let operator = "";
-let num2 = "";
+let previousValue = "";
+let currentValue = "";
 
-function operate(num1, num2, operator) {
-  switch (operator) {
-    case "+":
-      return add(num1, num2);
-    case "-":
-      return subtract(num1, num2);
-    case "*":
-      return multiply(num1, num2);
-    case "/":
-      return divide(num1, num2);
+document.addEventListener("DOMContentLoaded", function () {
+  let numbers = document.querySelectorAll(".button");
+  let operators = document.querySelectorAll(".button-operator");
+
+  let clear = document.querySelector(".button-clear");
+  let equal = document.querySelector(".button-equals");
+  let decimal = document.querySelector(".button-decimal");
+
+  let display = document.querySelector(".screen");
+
+  numbers.forEach((number) =>
+    number.addEventListener("click", function (e) {
+      handleNumber(e.target.textContent);
+      display.textContent = currentValue;
+    })
+  );
+
+  operators.forEach((op) =>
+    op.addEventListener("click", function (e) {
+      handleOperator(e.target.textContent);
+      display.textContent = currentValue + " " + operator;
+    })
+  );
+
+  clear.addEventListener("click", function () {
+    currentValue = "";
+    previousValue = "";
+    operator = "";
+    display.textContent = currentValue;
+  });
+
+  equal.addEventListener("click", function () {
+    if (currentValue != "" && previousValue != "") {
+      calculate();
+      display.textContent = "";
+      if (previousValue.length <= 5) {
+        display.textContent = previousValue;
+      } else {
+        display.textContent = previousValue.slice(0, 5) + "...";
+      }
+    }
+  });
+
+  decimal.addEventListener("click", function () {
+    addDecimal();
+  });
+});
+
+function handleNumber(num) {
+  if (currentValue.length <= 6) {
+    currentValue += num;
   }
 }
 
-// console.log(operate(3, 5, "*"));
+function handleOperator(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
+}
+
+function calculate() {
+  previousValue = Number(previousValue);
+  currentValue = Number(currentValue);
+
+  if (operator === "+") {
+    previousValue += currentValue;
+  } else if (operator === "-") {
+    previousValue -= currentValue;
+  } else if (operator === "x") {
+    previousValue *= currentValue;
+  } else if (operator === "/") {
+    previousValue /= currentValue;
+  }
+
+  previousValue = roundNumber(previousValue);
+  previousValue = previousValue.toString();
+}
+
+function roundNumber(num) {
+  return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal() {
+  if (!currentValue.includes(".")) {
+    currentValue += ".";
+  }
+}
